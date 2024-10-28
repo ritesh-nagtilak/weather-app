12d7ad4d4dbf6548593e164beffdcc14eb03319e
Ritesh Nagtilak <nagtilakritesh@gmail.com>
2024-10-28 23:41:23 +0530

    Initial commit of Weather App

---
+++ app.js
@@ -0,0 +1,28 @@
+const apiKey = '22081ea00b680d81a947bc33853e2271';
+
+function getWeather() {
+    const city = document.getElementById('cityInput').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("City not found. Please enter a valid city name.");
+            }
+            return response.json();
+        })
+        .then(data => displayWeather(data))
+        .catch(error => {
+            document.getElementById('weatherInfo').innerText = error.message;
+        });
+}
+
+function displayWeather(data) {
+    const { main, weather, name } = data;
+    document.getElementById('weatherInfo').innerHTML = `
+        <h2>${name}</h2>
+        <p>Temperature: ${main.temp}°C</p>
+        <p>Weather: ${weather[0].description}</p>
+        <p>Humidity: ${main.humidity}%</p>
+    `;
+}
